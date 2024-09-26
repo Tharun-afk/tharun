6fc59aad322b0d4e0e249dc1876fe4a29e3c1e5d
Tharun <tharunrachamalla5@gmail.com>
2024-09-26 10:27:03 +0530

    Add files via upload

---
+++ chitra.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {    
+    int number1= 10; 
+    int number2= 20;
+    int sum;
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
+
